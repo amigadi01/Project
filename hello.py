17e7c520bd155deeb983d8aa5daf7a15867aae59
Ami <ami.gadient@bluewin.ch>
2024-04-20 15:16:14 +0200

    anderung3

--- hello.py
+++ hello.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/user/')
+def userpage():
+    username = "Ami"
+    return render_template ("profil.html", username=username)
+
 @app.route('/about/') 
 def about():
     return render_template("about.html")
