6d8f4e15c12782c87ba4580ee1b5c57d5a19b490
Ami <ami.gadient@bluewin.ch>
2024-06-06 20:02:35 +0200

    anderung21

--- website/views.py
+++ website/views.py
@@ -1,61 +1,42 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
-from .models import Note
+from .models import Note  # oder ein eigenes Modell für Termine
 from . import db
 import json
 
 views = Blueprint('views', __name__)
 
+# Beispiel-Daten für den Kalender
 termin_daten = [
-     {
-            "title": 'event1',
-            "start": '2024-04-01'
-    },
-    {
-            "title": 'Ami Study',
-            "start": '2024-05-05T15:30:00',
-            "end": '2024-05-07'
-    },
-    {
-            "title": 'event3',
-            "start": '2024-01-09T12:30:00',
-            "allDay": False
-    }
+    {"id": 1, "title": 'Event 1', "start": '2024-04-01'},
+    {"id": 2, "title": 'Ami Study', "start": '2024-05-05T15:30:00', "end": '2024-05-07'},
+    {"id": 3, "title": 'Event 3', "start": '2024-01-09T12:30:00', "allDay": False}
 ]
 
-@views.route('/', methods=['GET', 'POST'])
-@login_required
-def home():
-    if request.method == 'POST':
-        note = request.form.get('note')
-        if len(note) < 1:
-            flash('Note is too short!', category='error')
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash('Note added!', category='success')
-    return render_template('home.html', user=current_user)
-
-@views.route('/delete-note', methods=['POST'])
-@login_required
-def delete_note():
-    note = json.loads(request.data)
-    note_id = note['noteId']
-    note = Note.query.get(note_id)
-    if note and note.user_id == current_user.id:
-        db.session.delete(note)
-        db.session.commit()
-    return jsonify({})
-
-@views.route('/calendar')
-def calendar():
-    return render_template('calendar.html', user=current_user)
-
-@views.route('/termine')
+@views.route('/termine', methods=['GET'])
 def termine():
     return jsonify(termin_daten)
 
-@views.route('/todo')
-def todo():
-    return render_template('todo.html', user=current_user)
+@views.route('/add-event', methods=['POST'])
+@login_required
+def add_event():
+    data = json.loads(request.data)
+    title = data['title']
+    start = data['start']
+    end = data.get('end')
+    
+    new_event = {"id": len(termin_daten) + 1, "title": title, "start": start, "end": end}
+    termin_daten.append(new_event)
+    
+    return jsonify(new_event)
+
+@views.route('/delete-event', methods=['DELETE'])
+@login_required
+def delete_event():
+    data = json.loads(request.data)
+    event_id = data['id']
+    
+    global termin_daten
+    termin_daten = [event for event in termin_daten if event["id"] != event_id]
+    
+    return jsonify({"success": True})
